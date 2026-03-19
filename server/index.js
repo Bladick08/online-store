d2926e771bb36d2005d666a69c2cf2aedc4ccd45
Bladick <boss.sava@outlook.com>
2026-03-19 23:21:59 +0100

    setup client and server

--- server/index.js
+++ server/index.js
@@ -1,11 +1,8 @@
+require("dotenv").config();
 const express = require("express");
+
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello bro 🚀");
-});
-
-app.listen(5000, () => {
-  console.log("Server started on port 5000");
-});
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
